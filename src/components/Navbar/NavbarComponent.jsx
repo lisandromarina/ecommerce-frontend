3cf3bf344409e901cc6d70f7c8ef854bd2ba8982
marianmarina4 <86482451+marianmarina4@users.noreply.github.com>
2022-06-10 14:42:28 -0300

    Add files via upload

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -7,25 +7,21 @@ function NavbarComponent() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="/">Mercado Licha</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Form className="d-flex">
-                            <FormControl 
-                                type="search"
-                                placeholder="Busca tu producto..."
-                                className="me-3"
-                                aria-label="Search"
-                            />
-                        </Form>
-                        <NavDropdown title="Categories" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Electrodomesticós</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Música</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Tecnología</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
+                <Nav className="flex-row justify-content-md-around w-100">
+                    <Navbar.Brand className="" href="/">Mercado Licha</Navbar.Brand>
+                    <Form className="d-flex col-6">
+                        <FormControl
+                            type="search"
+                            placeholder="Busca tu producto..."
+                            className="me-3"
+                            aria-label="Search"
+                        />
+                    </Form>
+                    <NavDropdown title="Create Account">
+                        <NavDropdown.Item href="#action/3.1">Sign Up</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Login</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
             </Container>
         </Navbar>
     )
