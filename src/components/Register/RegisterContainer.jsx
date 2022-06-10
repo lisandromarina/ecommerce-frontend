6b15929b8f3a43184b9d04ad60e0a3ec16e4b260
marianmarina4 <86482451+marianmarina4@users.noreply.github.com>
2022-06-10 14:41:34 -0300

    Add files via upload

---
+++ src/components/Register/RegisterContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RegisterComponent from "./RegisterComponent";
+
+function RegisterContainer() {
+    return (
+        <RegisterComponent />
+    )
+};
+
+export default RegisterContainer;
