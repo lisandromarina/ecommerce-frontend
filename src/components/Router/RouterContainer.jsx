5bc54bd2315ee3a3a4eb78400ef0de31f861d5e0
marianmarina4 <86482451+marianmarina4@users.noreply.github.com>
2022-06-11 20:53:19 -0300

    Add files via upload

--- src/components/Router/RouterContainer.jsx
+++ src/components/Router/RouterContainer.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from  "../HomePage";
+import HomePage from "../HomePage";
+import Register from "../Register";
+import LogIn from "../LogIn";
 
 function RouterContainer() {
     return (
         <Routes>
 
-            <Route path='/' element={<HomePage/>} />
-            
-        </Routes>  
+            <Route path='/' element={<HomePage />} />
+
+            <Route path='/register' element={<Register />} />
+
+            <Route path='/login' element={<LogIn />} />
+
+        </Routes>
     );
 }
-  
+
 export default RouterContainer;
