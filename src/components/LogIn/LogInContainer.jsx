7c3c781c4c4bb7bf57a6639f695dae1f4952854b
marianmarina4 <86482451+marianmarina4@users.noreply.github.com>
2022-06-11 20:52:25 -0300

    Add files via upload

---
+++ src/components/LogIn/LogInContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LogInComponent from "./LogInComponent";
+
+function LogInContainer() {
+    return (
+        <LogInComponent />
+    )
+};
+
+export default LogInContainer;
