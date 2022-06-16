441d92abf0a1fe24f70f6c5fc35ee25c013bf947
Lisandro Marina <lisandrom@epidata.net>
2022-06-16 15:08:56 -0300

    local storage for the token was implement

--- src/components/Navbar/NavbarContainer.jsx
+++ src/components/Navbar/NavbarContainer.jsx
@@ -1,22 +1,42 @@
-import React, { useState, handleOnChange } from 'react';
+import React, { useState, useEffect } from 'react';
+import { logOut, loggedIn } from "../../redux/slices/userSlice"
 import NavbarComponent from "./NavbarComponent";
+import { useSelector, useDispatch } from "react-redux";
+import { parseJwt } from '../../utils/tokenUtils';
 
 function NavbarContainer() {
-
     const [search, setSearch] = useState("");
+    const dispatch = useDispatch();
+    const userState = useSelector(state => state.user.user)
 
     function handleOnChange(text) {
         setSearch(text)
     };
-    function handleOnClick(){
+
+    function handleOnClick() {
 
     };
 
+    function handleOnLogOut() {
+        localStorage.clear();
+        dispatch(logOut())
+    };
+
+    useEffect(() => {
+        let token = localStorage.getItem("token")
+        if(token && !userState.usename){
+            let tokenParsed = parseJwt(token)
+            dispatch(loggedIn(tokenParsed.sub))
+        }
+      }, []);
+
     return (
         <NavbarComponent
-        handleOnChange={handleOnChange}
-        handleOnClick={handleOnClick}
-         />
+            userState={userState}
+            handleOnLogOut={handleOnLogOut}
+            handleOnChange={handleOnChange}
+            handleOnClick={handleOnClick}
+        />
     );
 }
 export default NavbarContainer;
