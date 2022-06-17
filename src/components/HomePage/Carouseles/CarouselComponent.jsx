5495c84a9dc7d870613c648548d8b29d530238d4
Lisandro Marina <lisandrom@epidata.net>
2022-06-17 10:39:53 -0300

    changed image height

--- src/components/HomePage/Carouseles/CarouselComponent.jsx
+++ src/components/HomePage/Carouseles/CarouselComponent.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 import { Carousel, Image } from 'react-bootstrap';
 
 function CarouselComponent() {
+    let INTERVAL_TIME = 10000; 
+    let IMAGE_HEIGHT = 300;
+
     return (
         <Carousel>
-            <Carousel.Item interval={10000000}>
+            <Carousel.Item interval={INTERVAL_TIME}>
                 <Image
-                    height={250}
+                    height={IMAGE_HEIGHT}
                     className="d-block w-100"
                     src={`${process.env.PUBLIC_URL}/assets/banner.jpg`}
                     alt="First slide"
                 />
             </Carousel.Item>
-            <Carousel.Item interval={500}>
+            <Carousel.Item interval={INTERVAL_TIME}>
                 <Image
-                    height={250}
+                    height={IMAGE_HEIGHT}
                     className="d-block w-100"
                     src={`${process.env.PUBLIC_URL}/assets/banner2.jpg`}
                     alt="Second slide"
                 />
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item interval={INTERVAL_TIME}>
                 <Image
-                    height={250}
+                    height={IMAGE_HEIGHT}
                     className="d-block w-100"
                     src={`${process.env.PUBLIC_URL}/assets/banner3.jpg`}
                     alt="Third slide"
