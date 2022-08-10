deeff5a4538f80bd17d52bdab1ab71626b7e6289
Lisandro Marina <lisandrom@epidata.net>
2022-08-09 23:30:50 -0300

    implemented banner

--- src/components/HomePage/HomePageComponent.jsx
+++ src/components/HomePage/HomePageComponent.jsx
@@ -19,6 +19,28 @@ function HomePageComponent(props) {
         src={`${process.env.PUBLIC_URL}/assets/miniBanner.jpeg`}
       />
       <Container>
+        <div className='d-flex justify-content-center'>
+          <Row md={3} xs={1} className='d-flex align-item-around' >
+            <Image
+              className='pt-5'
+              fluid={true}
+              width={"100%"}
+              src={`${process.env.PUBLIC_URL}/assets/category1.png`}
+            />
+            <Image
+              className='pt-5'
+              fluid={true}
+              width={"100%"}
+              src={`${process.env.PUBLIC_URL}/assets/category2.png`}
+            />
+            <Image
+              className='pt-5'
+              fluid={true}
+              width={"100%"}
+              src={`${process.env.PUBLIC_URL}/assets/category3.png`}
+            />
+          </Row>
+        </div>
         <div className='pt-5 pb-5'>
           <h2 className='text-center' style={{ color: "#e77800", fontFamily: "cursive" }}>DESTACADOS</h2>
           <Row md={4} xs={2} className='d-flex align-item-around' >
