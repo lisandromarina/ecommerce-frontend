8d92cf90b9d7239d8c20ef8fe6cdcad9a9a07f0e
marianmarina4 <marianomarina04@hotmail.com>
2023-01-19 22:03:58 -0300

    change responsive register

--- src/views/Register/RegisterComponent.jsx
+++ src/views/Register/RegisterComponent.jsx
@@ -12,38 +12,34 @@ function RegisterComponent(props) {
         handleOnSubmit
     } = props;
     return (
-        <Container>
-            <div className="div-wrapper-register">
-                <div className="div-wrapper-form-register">
-                    <div required className="div-form-register">
-                        <h2 className="div-title-register">Create your account</h2>
-                        {/* NAME  */}
-                        <label className="label-register" for="name">Name
-                            <input onChange={HandleOnChange} name="name" type="text" className="input-register" placeholder="First and last name" /></label>
-                        {/* USERNAME  */}
-                        <label className="label-register" for="username">Username
-                            <input onChange={HandleOnChange} name="username" type="text" class="input-register" placeholder="MercadoLicha" /></label>
-                        {/* EMAIL  */}
-                        <label className="label-register" for="email">Email address
-                            <input onChange={HandleOnChange} name="email" type="email" class="input-register" placeholder="example@hotmail.com" /></label>
-                        {/* PASSWORD  */}
-                        <label className="label-register" for="password" >Password
-                            <input onChange={HandleOnChange} name="password" type="password" class="input-register" placeholder="*******" /></label>
-                        {/* CONFIRM PASSWORD  */}
-                        <label className="label-register" for="password" >Confirm Password
-                            <input onChange={HandleOnChange} name="password" type="password" class="input-register" placeholder="*******" /></label>
-                        {/* TERMS AND CONDITIONS  */}
-                        <label class="label-register" for="termsConditions">Accept terms and conditions
-                            <input required type="checkbox" name="termsConditions" /></label>
-                        {/* SUBMIT BUTTON  */}
-                        <input type="submit" onClick={() => handleOnSubmit()} className="input-register-submit" value="Submit" />
-                        {/* HAVE ACCOUNT  */}
-                        <p className="p-register">
-                            Do you already have an account?
-                            <a href="login" className="a-register">Log in</a>
-                        </p>
-                    </div>
-                </div>
+        <Container className="register-wrapper">
+            <div required className="register-form">
+                <h2 className="register-title">Crea tu cuenta</h2>
+                {/* NAME  */}
+                <label className="register-label" for="name">Nombre
+                    <input onChange={HandleOnChange} name="name" type="text" className="register-input" placeholder="Nombre y apellido" /></label>
+                {/* USERNAME  */}
+                <label className="register-label" for="username">Usuario
+                    <input onChange={HandleOnChange} name="username" type="text" class="register-input" placeholder="MercadoLicha" /></label>
+                {/* EMAIL  */}
+                <label className="register-label" for="email">Correo Electronico
+                    <input onChange={HandleOnChange} name="email" type="email" class="register-input" placeholder="ejemplo@hotmail.com" /></label>
+                {/* PASSWORD  */}
+                <label className="register-label" for="password" >Contraseña
+                    <input onChange={HandleOnChange} name="password" type="password" class="register-input" placeholder="*******" /></label>
+                {/* CONFIRM PASSWORD  */}
+                <label className="register-label" for="password" >Confirmar Contraseña
+                    <input onChange={HandleOnChange} name="password" type="password" class="register-input" placeholder="*******" /></label>
+                {/* TERMS AND CONDITIONS  */}
+                <label class="register-terms" for="termsConditions">Aceptar terminos y condiciones
+                    <input required type="checkbox" name="termsConditions" /></label>
+                {/* SUBMIT BUTTON  */}
+                <input type="submit" onClick={() => handleOnSubmit()} className="register-submit" value="Enviar" />
+                {/* HAVE ACCOUNT  */}
+                <p className="p-register">
+                    Ya estas registrado?
+                    <a href="login" className="a-register">Log in</a>
+                </p>
             </div>
         </Container>
     )
