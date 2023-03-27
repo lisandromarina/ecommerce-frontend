49c0005946b0ec7e625a9e163ac5d3c1c5a124f6
Lisandro <lisandro.marina@bweventstech.com>
2023-03-27 17:20:13 -0300

    improve styles

--- src/components/ListProducts/ListProductsComponents.jsx
+++ src/components/ListProducts/ListProductsComponents.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
-import CardHomePage from "../../components/CardHomePage"
+import CardHomePage from "../../components/CardHomePage";
 
 function ListProductsContainer(props) {
     const {
@@ -15,7 +15,7 @@ function ListProductsContainer(props) {
                 <h2 className='text-center' style={{ color: "#e77800", fontFamily: "Space Grotesk" }}>
                     {title}
                 </h2>
-                <Row md={4} xs={2} className='d-flex align-item-around justify-content-center' >
+                <Row md={4} xs={2} className='d-flex align-item-around justify-content-start' >
                     {
                         arrayProducts.map(product => (
                             <CardHomePage
