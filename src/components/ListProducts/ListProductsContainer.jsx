e179b8de9e82a2e8087fb1710792710df2b630f8
Lisandro <lisandro.marina@bweventstech.com>
2023-06-15 16:06:47 -0400

    create hook to the responsive items and change list products

--- src/components/ListProducts/ListProductsContainer.jsx
+++ src/components/ListProducts/ListProductsContainer.jsx
@@ -1,53 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ListProductsComponent from "./ListProductsComponents";
 import usePageable from '../../hook/usePageable';
+import useResponsiveItemsQuantity from '../../hook/useResponsiveItemsQuantity.jsx'
 
 function ListProductsContainer(props) {
-    const {
-        arrayProducts,
-        title,
-        itemsQuantity,
-    } = props;
-    const [itemQuantity, setItemsQuantity] = useState(itemsQuantity)
-    const { currentPage, goToPage, paginateItems, resetPage } = usePageable(itemQuantity);
+    const { arrayProducts, title } = props;
     const navigate = useNavigate();
+    const itemsQuantity = useResponsiveItemsQuantity();
+    const { currentPage, goToPage, paginateItems, resetPage } = usePageable(itemsQuantity);
 
-    let currentItems = paginateItems(arrayProducts);
-    const totalPages = Math.ceil(arrayProducts.length / itemQuantity);
-
+    const currentItems = paginateItems(arrayProducts);
+    const totalPages = Math.ceil(arrayProducts.length / itemsQuantity);
 
     function handleOnClick(productId) {
-        navigate(`/product/${productId}`)
+        navigate(`/product/${productId}`);
     }
 
-    useEffect(()=> {
-        resetPage()
-    }, [arrayProducts,/*  window.innerWidth */])
-
-    useEffect(()=> {
-        const handleResize = () => {
-            const screenWidth = window.innerWidth;
-      
-            if (screenWidth < 480) {
-                setItemsQuantity(4)
-            } else if (screenWidth > 481 & screenWidth < 768) {
-                setItemsQuantity(4)
-            } else if (screenWidth > 769 & screenWidth < 1200) {
-                setItemsQuantity(6)
-            } else {
-                setItemsQuantity(8)
-            }
-          };
-      
-          handleResize();
-      
-          window.addEventListener('resize', handleResize);
-      
-          return () => {
-            window.removeEventListener('resize', handleResize);
-          };
-        }, []);
+    useEffect(() => {
+        resetPage();
+    }, [arrayProducts]);
 
     return (
         <ListProductsComponent
@@ -58,7 +30,7 @@ function ListProductsContainer(props) {
             currentPage={currentPage}
             totalPages={totalPages}
         />
-    )
+    );
 };
 
 export default ListProductsContainer;
