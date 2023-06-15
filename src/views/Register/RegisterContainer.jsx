78a25465f3eb76bcdbdf9d5f075c6c1bad4c920e
Lisandro <lisandro.marina@bweventstech.com>
2023-06-15 15:46:55 -0400

    deleted comment in register

--- src/views/Register/RegisterContainer.jsx
+++ src/views/Register/RegisterContainer.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import RegisterComponent from "./RegisterComponent";
 import { useDispatch } from "react-redux";
 import { register } from "../../redux/actions/userAction"
-/* import { useNavigate, useLocation } from 'react-router-dom'; */
 import useFormState from '../../hook/useFormState';
 
-
 function RegisterContainer() {
-   /*  const navigate = useNavigate() */
     const dispatch = useDispatch();
-   /*  const location = useLocation(); */
 
     const initialState = {
         firstName: undefined,
@@ -21,7 +17,6 @@ function RegisterContainer() {
     const { formData: user, handleChange: HandleOnChange } = useFormState(initialState);
 
     function handleOnSubmit() {
-        /* const prevPath = location.state.prevPath;  ADD NAVIGATE TO HOMEPAGE*/
         dispatch(register(user));
     }
 
