5c2d0cfcc457d18049d5eedac6f5f42f47f0b252
Lisandro <lisandro.marina@bweventstech.com>
2023-06-26 13:26:32 -0400

    add origin header in api

--- src/api/authHeader.js
+++ src/api/authHeader.js
@@ -2,8 +2,8 @@ export function authHeader() {
   // return authorization header with jwt token
   let token = localStorage.getItem('token');
   if (token) {
-    return { 'Authorization': `Bearer ${token}` };
+    return { 'Authorization': `Bearer ${token}`, 'Access-Control-Allow-Origin': '*' };
   } else {
-    return {};
+    return {'Access-Control-Allow-Origin': '*'};
   }
 }
